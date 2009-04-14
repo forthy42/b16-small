08e6188d8890f934fe4734184b8cd68486e71a67
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-04-14 21:12:04 +0000

    Small fix to work better
    git-svn-id: https://www.forth-ev.de/repos/b16-small@1159 3b8d8251-53f3-0310-8f3b-fd1cb8370982

--- b16.m
+++ b16.m
@@ -29,7 +29,7 @@ class;
 
 b16-state implements
  ( [methodstart] ) \ : assign drop ;
-: update  stopped @ 0= ?EXIT  load-regs
+: update  stopped @ 0= ?EXIT  load-regs stopped dup @ >r off
   regs 0 + w@ 0 p# assign
   regs 2 + w@ 0 t# assign
   regs 4 + w@ 0 r# assign
@@ -42,18 +42,19 @@ b16-state implements
   stack 8 + w@ 0 s2# assign
   stack 10 + w@ 0 r2# assign
   stack 12 + w@ 0 s3# assign
-  stack 14 + w@ 0 r3# assign ;
+  stack 14 + w@ 0 r3# assign r> stopped ! ;
 : show  '$' 0
   2dup p# keyed  2dup t# keyed  2dup r# keyed  2dup s# keyed
   2dup s0# keyed 2dup s1# keyed 2dup s2# keyed 2dup s3# keyed
   2dup r0# keyed 2dup r1# keyed 2dup r2# keyed 2dup r3# keyed
-  i# keyed  super show ; ( [methodend] ) 
+  i# keyed  status@ 1 and 0= dup stopped !
+  IF  update  THEN  super show ; ( [methodend] ) 
   : widget  ( [dumpstart] )
             #0. ]N ( MINOS ) ^^ SN[ stopped @ 0= ?EXIT  p# get drop DBG_P u! ]SN ( MINOS ) X" P" infotextfield new  ^^bind p#
             #0. ]N ( MINOS ) ^^ SN[ stopped @ 0= ?EXIT  i# get drop DBG_I u! ]SN ( MINOS ) X" I" infotextfield new  ^^bind i#
             #0. ]N ( MINOS ) ^^ SN[ stopped @ 0= ?EXIT  s# get drop DBG_STATE u! ]SN ( MINOS ) X" S" infotextfield new  ^^bind s#
           #3 hatbox new #1 hskips
-            ^^ TV[ stopped ]T[ ( MINOS ) stopped @ IF  b16-stop update  ELSE  b16-run  THEN ]TV ( MINOS )  2icon" icons/stop"icons/play" toggleicon new  ^^bind stoptoggle
+            ^^  0 T[ stopped on b16-stop update ][ ( MINOS ) stopped off b16-run ]T ( MINOS )  2icon" icons/stop"icons/play" toggleicon new  ^^bind stoptoggle
             ^^ S[ steps# get ?DO  b16-step update I 1+ 0 steps# assign  LOOP ]S ( MINOS )  icon" icons/step" icon-but new 
             #1. ]N ( MINOS ) ^^ SN[  ]SN ( MINOS ) textfield new  ^^bind steps#
           #3 habox new hfixbox  #1 hskips
