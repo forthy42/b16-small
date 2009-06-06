1c56e1aeb4e20fd0edb1b1a53db9bf45acbc9495
bp <bp@3b8d8251-53f3-0310-8f3b-fd1cb8370982>
2009-06-06 16:35:39 +0000

    Square root
    git-svn-id: https://www.forth-ev.de/repos/b16-small@1248 3b8d8251-53f3-0310-8f3b-fd1cb8370982

---
+++ sqrt.asm
@@ -0,0 +1,36 @@
+macro: swap ( a b -- b a ) over >r nip r> end-macro
+macro: ! ( a b -- )  !. drop  end-macro
+include regmap.asm
+$2000 org
+
+macro: r@ r> dup >r end-macro
+macro: 2* dup + end-macro
+macro: 2*c dup +c end-macro
+: t4*
+    >r >r 2* r> 2*c r> 2*c
+    >r >r 2* r> 2*c r> 2*c ;
+: sqr- 2* >r t4* r@ 2* 1 # + com +c dup
+    cIF  r> 1 # + >r  ELSE  r@ 2* 1 # + +  THEN
+r> ;
+: sqrt 0 # dup
+    sqr-
+    sqr-
+    sqr-
+    sqr-
+    sqr- sqr- sqr- sqr-
+    sqr- sqr- sqr- sqr-
+    sqr- sqr- sqr- sqr-
+    nip nip nip ;
+: boot
+    BEGIN
+        4 # 0 # sqrt
+        drop
+        16 # 0 # sqrt
+        drop
+        $5678 # $1234 # sqrt
+        drop
+    AGAIN ;
+$3FFE org
+     boot ;;
+
+
